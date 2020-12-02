26273c27828470c40f7434e44209851a0035f7c9
Jenny Epstein <jeepstein@chapman.edu>
2020-12-02 10:49:24 -0800

    Changed logic of my while statement to allow for user input

--- cars.sh
+++ cars.sh
@@ -2,13 +2,17 @@
 #cars.sh
 #Jenny Epstein
 #December 2, 2020
-echo "Enter 1 to continue to menu, or enter 0 to quit "
-read n
-i=3
-while [ "$n" -lt "$i" ]
+e=3
+while [ "$n" -lt "$e" ]
 do 
 	echo "Enter the number 1 to enter a new car"
 	echo "Enter the number 2 to display the list of cars"
 	echo "Enter the number 3 to quit and exit the program"
+	read n;
+	case "$n" in
+		"1") echo "Enter a year, a make, and a model";;
+		"2") echo cat My_old_cars.txt;;
+		"3") echo "Goodbye!";;
+	
 done
 
