77454a011275cea34fdd1d66e6227eb343f46db5
Jenny Epstein <jeepstein@chapman.edu>
2020-12-02 10:28:55 -0800

    add the while statement in program to display menu options

--- cars.sh
+++ cars.sh
@@ -2,4 +2,13 @@
 #cars.sh
 #Jenny Epstein
 #December 2, 2020
+echo "Enter 1 to continue to menu, or enter 0 to quit "
+read n
+i=3
+while [ "$n" -lt "$i" ]
+do 
+	echo "Enter the number 1 to enter a new car"
+	echo "Enter the number 2 to display the list of cars"
+	echo "Enter the number 3 to quit and exit the program"
+done
 
