15b47eab2a3412c83a26425b4b9b60dd241a6786
Jenny Epstein <jeepstein@chapman.edu>
2020-12-02 17:46:26 -0800

    changed the condition for the first case statement

--- cars.sh
+++ cars.sh
@@ -9,8 +9,13 @@ do
 	echo "Enter the number 2 to display the list of cars"
 	echo "Enter the number 3 to quit and exit the program"
 	read n;
+	car =""
 	case "$n" in
-		"1") echo "Enter a year, a make, and a model";;
+		"1") echo "Enter a year"; read y;;
+		     echo "Enter a make"; read m;;
+		     echo "Enter a model" read o;;
+		     car +=y:m:o
+		     echo car >> My_old_cars.txt
 		"2") echo cat My_old_cars.txt;;
 		"3") echo "Goodbye!";;
 	
