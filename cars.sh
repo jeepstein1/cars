ec8acae61dd9baa9c3c7b9adc5d2f8abda665ddd
Jenny Epstein <jeepstein@chapman.edu>
2020-12-02 18:04:13 -0800

    worked on exiting an infinite loop

--- cars.sh
+++ cars.sh
@@ -9,12 +9,15 @@ echo "Enter the number 3 to quit and exit the program"
 read n;
 while [ "$n" -lt "$e" ]
 do 
-	$car =""
+	
 	case "$n" in
-		"1") echo "Enter a year"; read y
-		     echo "Enter a make"; read m
-		     echo "Enter a model" read o
-		     car +=y:m:o
+		"1") echo "Enter a year";
+		     read y
+		     echo "Enter a make";
+ 		     read m
+		     echo "Enter a model";
+		     read o
+		     $car +=y:m:o
 		     echo car >> My_old_cars.txt;;
 		"2") echo cat My_old_cars.txt;;
 		"3") echo "Goodbye!";;
