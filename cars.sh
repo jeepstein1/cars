f6316f69338d4b1bae1163e9b32ae51d6fd5b7a0
Jenny Epstein <jeepstein@chapman.edu>
2020-12-02 17:50:25 -0800

    fixed case statement syntax error

--- cars.sh
+++ cars.sh
@@ -11,11 +11,11 @@ do
 	read n;
 	car =""
 	case "$n" in
-		"1") echo "Enter a year"; read y;;
-		     echo "Enter a make"; read m;;
-		     echo "Enter a model" read o;;
+		"1") echo "Enter a year"; read y
+		     echo "Enter a make"; read m
+		     echo "Enter a model" read o
 		     car +=y:m:o
-		     echo car >> My_old_cars.txt
+		     echo car >> My_old_cars.txt;;
 		"2") echo cat My_old_cars.txt;;
 		"3") echo "Goodbye!";;
 	
