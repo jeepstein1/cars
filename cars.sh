226bb1cb789bd8f027dbc9c71e3cdf890c294e0a
Jenny Epstein <jeepstein@chapman.edu>
2020-12-02 22:37:35 -0800

    added -r to the read commands and changes position of $ in line 23

--- cars.sh
+++ cars.sh
@@ -10,17 +10,17 @@ do
 	echo "Enter the number 1 to enter a new car"
 	echo "Enter the number 2 to display the list of cars"
 	echo "Enter the number 3 to quit and exit the program"
-	read n
+	read -r n
 	case "$n" in
 		"1") echo "Enter a year"
-		     read y
+		     read -r y
 		     echo "Enter a make"
- 		     read m
+ 		     read -r m
 		     echo "Enter a model"
-		     read o
+		     read -r o
 		     string="$y:$m:$o"
 	
-		     echo $"string" >> My_old_cars.txt;;
+		     echo "$string" >> My_old_cars.txt;;
 		"2") sort My_old_cars.txt;;
 		"3") echo "Goodbye!";;
 	esac
